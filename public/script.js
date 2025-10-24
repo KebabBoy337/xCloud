e76c37e7413e12980895d1137a6a38983e83a7b0
KebabBoy337 <arsenbond2001@gmail.com>
2025-10-24 21:39:20 +0200

    Update version to 1.5.1 and adjust UI elements for improved layout and responsiveness. Modified CSS for better spacing and alignment, and updated version references in HTML and JavaScript files.

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ class xCloudStorage {
         this.files = [];
         this.folders = [];
         this.currentFolder = '';
-        this.version = '1.5.0'; // Версия приложения
+        this.version = '1.5.1'; // Версия приложения
         this.selectedFiles = new Set(); // Для отслеживания выбранных файлов
         this.selectedFolders = new Set(); // Для отслеживания выбранных папок
         this.init();
